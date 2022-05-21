9d688f9da7a79e48ae2d32fb3acf9544ab107e4b
qinry <lixiaoqin28@outlook.com>
2022-05-21 23:56:33 +0800

    update lca in tree

--- java/src/main/java/io/dure/tree/LowestCommonAncestor.java
+++ java/src/main/java/io/dure/tree/LowestCommonAncestor.java
@@ -1,27 +1,59 @@
 package io.dure.tree;
 
+import java.util.*;
+
 public class LowestCommonAncestor {
     // 二叉树
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return dfs(root, p, q);
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        Map<Integer, TreeNode> parent = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                if (curr.left != null) {
+                    parent.put(curr.left.val, curr);
+                }
+                curr = curr.left;
+
+            }
+            curr = stack.pop();
+            if (curr.right != null) {
+                parent.put(curr.right.val, curr);
+            }
+            curr = curr.right;
+
+        }
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
     }
 
-    private TreeNode dfs(TreeNode x, TreeNode p, TreeNode q) {
-        if (x == null || x.val == p.val || x.val == q.val) {
-            return x;
-        }
-        TreeNode left = dfs(x.left, p, q);
-        TreeNode right = dfs(x.right, p, q);
-
-        if (left == null) {
-            return right;
-        }
-
-        if (right == null) {
-            return left;
-        }
-        return x;
-    }
+//    private TreeNode dfs(TreeNode x, TreeNode p, TreeNode q) {
+//        if (x == null || x.val == p.val || x.val == q.val) {
+//            return x;
+//        }
+//        TreeNode left = dfs(x.left, p, q);
+//        TreeNode right = dfs(x.right, p, q);
+//
+//        if (left == null) {
+//            return right;
+//        }
+//
+//        if (right == null) {
+//            return left;
+//        }
+//        return x;
+//    }
 
     // 二叉搜索树
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
